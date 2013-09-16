36cd48f5302b22681d1c6e5060d9d45645b8634d
Wil Reichert <wil.reichert@kt.com>
2013-09-16 12:23:18 +0900

    use new utils

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -9,11 +9,15 @@
 
 include_recipe 'logrotate'
 
+rotate_cmd = "\tif [ -d /etc/logrotate.d/httpd-prerotate ]; then "
+rotate_cmd << "run-parts /etc/logrotate.d/httpd-prerotate; "
+rotate_cmd << "fi;"
+
 logrotate_app "apache2" do
   path "/var/log/apache2/*.log"
   rotate 4
   create "640 root adm"
   sharedscripts true
   postrotate "\t/etc/init.d/apache2 reload > /dev/null"
-  prerotate "\tif [ -d /etc/logrotate.d/httpd-prerotate ]; then run-parts /etc/logrotate.d/httpd-prerotate; fi;" 
+  prerotate rotate_cmd
 end
