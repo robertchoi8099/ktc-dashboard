10afdb9fc1a0bb553b47fe0896a734614c4d0a8b
Wil Reichert <wil.reichert@kt.com>
2013-11-06 11:13:55 +0900

    register horizon service with etcd.  this is not technically an openstack enpoint and must be treated separately

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,7 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# http://jira.ktcloudware.com/browse/UCLOUDNG-588 
+include_recipe "services"
+include_recipe "ktc-utils"
+
+ip = KTC::Network.address "management"
+
+Services::Connection.new run_context: run_context
+horizon = Services::Member.new node[:fqdn],
+  service: "horizon",
+  port: 443,
+  proto: "tcp",
+  ip: ip
+
+horizon.save
+
+# http://jira.ktcloudware.com/browse/UCLOUDNG-588
 # Add response time format to apache log format
 chef_gem "chef-rewind"
 require 'chef/rewind'
